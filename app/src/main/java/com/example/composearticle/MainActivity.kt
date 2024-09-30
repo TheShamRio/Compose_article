e5d6988eb87fbe1e2f5e1f1be9eb01368c2d0050
TheShamRio <xsham@inbox.ru>
2024-09-30 19:27:08 +0300

    Add basic ArticleCard component

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -28,5 +29,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun ArticleCard(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        // Components will be added here later
     }
 }
